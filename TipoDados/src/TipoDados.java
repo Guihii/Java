406005a1e726e2ba0d58eb69b1be723d92c0c441
Guihii <guihiirodrigues@gmail.com>
2022-08-16 16:15:54 -0300

    variaveis

---
+++ TipoDados/src/TipoDados.java
@@ -0,0 +1,22 @@
+public class TipoDados {
+    public static void main(String[] args)  {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; //se começar com 0 talvez tenha qie ser outro tipo
+        long cpf = 987654322109L; //se começar com 0 talvez tenha qie ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Idade: " + ano);
+        System.out.println("Idade: " + cep);
+        System.out.println("Idade: " + cpf);
+        System.out.println("Idade: " + pi);
+        System.out.println("Idade: " + salario);
+
+        
+
+
+
+    }
+}
