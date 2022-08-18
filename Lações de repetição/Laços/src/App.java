24f7a9b8b92f44430fb5241be30115ff8979b62d
Guihii <guihiirodrigues@gmail.com>
2022-08-18 15:46:57 -0300

    While

---
+++ Lações de repetição/Laços/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int idade = 15;
+
+
+    while (idade < 18){
+        System.out.println(idade);
+        idade = idade + 1;
+    }
+    }
+}
