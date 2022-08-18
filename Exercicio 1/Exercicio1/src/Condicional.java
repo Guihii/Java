f5cd53d73fe23f7819c9b6770f40d3a3cdc20d88
Guihii <guihiirodrigues@gmail.com>
2022-08-18 13:41:46 -0300

    Condicionai sem o if/else

---
+++ Exercicio 1/Exercicio1/src/Condicional.java
@@ -0,0 +1,25 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+
+        Boolean SimNao1 = numero1 == numero2;
+        Boolean SimNao2 = numero1 != numero2;
+        Boolean SimNao3 = numero1 > numero2;
+        Boolean SimNao4 = numero1 < numero2;
+        Boolean SimNao5 = numero1 <= numero2;
+        Boolean SimNao6 = numero1 >= numero2;
+
+        System.out.println(SimNao1);
+        System.out.println(SimNao2);
+        System.out.println(SimNao3);
+        System.out.println(SimNao4);
+        System.out.println(SimNao5);
+        System.out.println(SimNao6);
+
+
+
+
+    }
+}
